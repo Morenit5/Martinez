6fd4cb43aefd57670473905c777362435bbd4905
Yesi <rejon.yesenia@gmail.com>
2025-09-29 01:30:50 -0600

    Cambios en Invoice-Services.

--- backend/src/services/Email.service.ts
+++ backend/src/services/Email.service.ts
@@ -5,6 +5,7 @@ import * as path from 'path';
 import { PDFDocument, rgb, StandardFonts } from 'pdf-lib';
 import { EntityService } from 'src/entities/Service.entity';
 import * as fs from 'fs';
+import { ServiceDetailDto } from 'src/dto/ServiceDetail.dto';
 
 enum EmailOptions {
   subject = "Envío de Factura Martínez",
@@ -47,7 +48,7 @@ export class EmailService {
   }
 
   async sendMail(entity: EntityService) {
-    var variable: EntityService= plainToInstance(EntityService,entity);
+    var variable: EntityService = plainToInstance(EntityService, entity);
 
     //var correo = JSON.stringify(entity.client.email);
     //console.log('CORREOoooooooooooo: '+correo);
@@ -84,24 +85,16 @@ export class EmailService {
     return 'Hello World!';
   }
 
+
+
   async generateInvoice(service: EntityService) {
-    var variable: EntityService= plainToInstance(EntityService, service);
 
-   // Crear documento PDF
+    // Crear documento PDF
     const pdfDoc = await PDFDocument.create();
-    const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
-
-    // 📋 Datos de ejemplo (productos)
-    const productos = Array.from({ length: 60 }, (_, i) => ({
-      id: i + 1,
-      nombre: `Service ${i + 1}`,
-      cantidad: Math.floor(Math.random() * 5) + 1,
-      precio: (Math.random() * 500 + 100).toFixed(2),
-    }));
 
     // Configuración de página
-    const pageWidth = 612;//595;   // A4 ancho
-    const pageHeight = 792;//842;  // A4 alto
+    const pageWidth = 612;   // A4 ancho - 595
+    const pageHeight = 792;  // A4 alto - 842
     const margin = 70;
     const lineHeight = 20;
     const maxRowsPerPage = 25; // filas por página
@@ -109,168 +102,207 @@ export class EmailService {
     let currentY = pageHeight - margin - 80; // espacio para encabezado
     let rowCount = 0;
 
+
+    function wrapText(text: string, font: any, size: number, maxWidth: number): string[] {
+      const words = text.split(' ');
+      const lines: string[] = [];
+      let currentLine = '';
+
+      words.forEach(word => {
+        const testLine = currentLine ? currentLine + ' ' + word : word;
+        const textWidth = font.widthOfTextAtSize(testLine, size);
+
+        if (textWidth > maxWidth) {
+          lines.push(currentLine);
+          currentLine = word;
+        } else {
+          currentLine = testLine;
+        }
+      });
+
+      if (currentLine) lines.push(currentLine);
+      return lines;
+    }
+
     // Crear primera página
     let page = pdfDoc.addPage([pageWidth, pageHeight]);
 
-//
- // Crear documento PDF
-// Si tienes un logo en base64 puedes importarlo o pegarlo aquí
-const logoBase64 = "data:image/png;base64,..."; // coloca aquí tu logo en base64
-  const { height } = page.getSize();
+    // Crear documento PDF
+    // Si tienes un logo en base64 puedes importarlo o pegarlo aquí
+    const logoBase64 = "data:image/png;base64,..."; // coloca aquí tu logo en base64
+    const { height } = page.getSize();
+    let y = height - 50;
 
-  let y = height - 50;
+    // Fuentes
+    const timesRoman = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+    const timesRomanBold = await pdfDoc.embedFont(StandardFonts.TimesRomanBold);
 
-  // === Logo ===
-  /*if (logoBase64) {
-    const logoBytes = Uint8Array.from(atob(logoBase64.split(',')[1]), c => c.charCodeAt(0));
+    async function loadImage(url: string): Promise<Uint8Array> {
+  const response = await fetch(url);
+  const arrayBuffer = await response.arrayBuffer();
+  return new Uint8Array(arrayBuffer);
+}
+
+    // === Cargar logo desde backend/assets/logo.png ===
+    const logoPath: string = path.join(__dirname, '..', 'assets','logo.png');
+    const logoBytes = fs.readFileSync(logoPath);
     const logoImage = await pdfDoc.embedPng(logoBytes);
+    const logoDims = logoImage.scale(0.04);
+
+    // Insertar logo
     page.drawImage(logoImage, {
-      x: 50,
-      y: y - 60,
-      width: 100,
-      height: 50,
-    });
-  }*/
-
-  // === Encabezado ===
-    page.drawText('INVOICE', { x: margin+290, y: pageHeight - margin, size: 45, color: rgb(0, 0.6, 0), // verde
+      x: 60,
+      y:  height -90,
+      width: logoDims.width,
+      height: logoDims.height,
     });
 
-  // Línea verde bajo el título
+    // === Encabezado ===
+    page.drawText('INVOICE', {
+      x: margin + 275, y: pageHeight - margin, size: 45, color: rgb(0, 0.6, 0), font: timesRomanBold // verde
+    });
+
+    // Línea verde bajo el título
     page.drawLine({
       start: { x: margin, y: pageHeight - margin - 15 },
       end: { x: pageWidth - margin, y: pageHeight - margin - 15 },
       thickness: 2,
       color: rgb(0, 0.4, 0),
     });
-
-        page.drawLine({
-  start: { x: 0, y: 70 },       // inicio (abajo)
-  end: { x: 0, y: pageHeight-70 }, // fin (arriba)
-  thickness: 20,
-  color: rgb(0,0.6,0), // verde
-});
-        page.drawLine({
-  start: { x: 0, y: 0 },       // inicio (abajo)
-  end: { x: 0, y: pageHeight }, // fin (arriba)
-  thickness: 40,
-  color: rgb(0, 0.6, 0), // verde
-});
-
-  y -= 60;
-  // === Datos del cliente ===
-  page.drawText("INVOICE TO:", { x: 70, y, size: 12 });
-  y -= 15;
-  page.drawText("Client: "+ variable.client?.name + ' ' + service.client.lastName/**/, { x: 70, y, size: 10 });
-  y -= 15;
-   page.drawText("RFC: JUPX800101XXX", { x: 70, y, size: 10 });
-  y -= 15;
-  page.drawText("Address: "/* + entity.client.address*/, { x: 70, y, size: 10 });
-
-  // === Datos de la empresa ===
-  y -= -45;//40;
-  page.drawText("COMPANY:", { x: 480, y, size: 12, font: undefined });
-  y -= 15;
-  page.drawText("RFC: XYZ010101ABC", { x: 440, y, size: 10 });
-  y -= 15;
-  page.drawText("Dirección: Calle Falsa 456, Ciudad", { x: 390, y, size: 10 });
-
-
-   // === Tabla de productos ===
-  y -= 70;
-  const headers = ['Type', 'Description', 'Unit Price', 'Quantity', 'Price'];
-  const rows = [
-    ['Laptop', '1','10000', '$15,000.00', '$15,000.00'],
-    ['Mouse', '2','10000', '$300.00', '$600.00'],
-    ['Teclado', '1','10000', '$500.00', '$500.00'],
-  ];
-
-  const colWidths = [100, 80, 100, 100, 100];
-  const rowHeight = 25;
-  let startX = 70;
-
- // Dibujar encabezados
-  let currentX = startX;
-  headers.forEach((header, i) => {
-    page.drawRectangle({
-      x: currentX,
-      y: y - rowHeight,
-      width: colWidths[i],
-      height: rowHeight,
-      borderColor: rgb(0, 0, 0),
-      borderWidth: 1,
+    page.drawLine({
+      start: { x: 0, y: 70 },       // inicio (abajo)
+      end: { x: 0, y: pageHeight - 70 }, // fin (arriba)
+      thickness: 20,
+      color: rgb(0, 0.6, 0), // verde
     });
-    page.drawText(header, { x: currentX + 5, y: y - 18, size: 12 });
-    currentX += colWidths[i];
-  });
+    page.drawLine({
+      start: { x: 0, y: 0 },       // inicio (abajo)
+      end: { x: 0, y: pageHeight }, // fin (arriba)
+      thickness: 40,
+      color: rgb(0, 0.6, 0), // verde
+    });
 
-  y -= rowHeight;
+    //
+    const meses = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+    const fecha = new Date();
+    const mesActual = meses[fecha.getMonth()]; // getMonth() devuelve 0-11
+    let fechaActual = mesActual + ` ${fecha.getDate()}, ${fecha.getFullYear()}`;
 
-  // Dibujar filas
-  rows.forEach(row => {
-    currentX = startX;
-    row.forEach((cell, i) => {
+    y -= 60;
+    // === Datos del cliente ===
+    page.drawText("INVOICE TO:", { x: 70, y, size: 12, font: timesRomanBold });
+    y -= 15;
+    page.drawText("Client: " + service.client?.name + ' ' + service.client.lastName/**/, { x: 70, y, size: 10, font: timesRoman });
+    y -= 15;
+    page.drawText("Phone: " + service.client.phone, { x: 70, y, size: 10, font: timesRoman });
+    y -= 15;
+    page.drawText("Address: " + service.client.address, { x: 70, y, size: 10, font: timesRoman });
+    y -= 15;
+    page.drawText("Invoice Date: " + fechaActual, { x: 70, y, size: 10, font: timesRoman });
+
+
+    // Assuming you have a UUID string
+    const invoiceNumber = service.invoice?.invoiceNumber.substring(service.invoice?.invoiceNumber.lastIndexOf('-') + 1, service.invoice?.invoiceNumber.length);
+
+    // === Datos de la empresa ===
+    y -= -60;//40;
+    page.drawText("COMPANY:", { x: 415, y, size: 12, font: timesRomanBold });
+    y -= 15;
+    page.drawText("MARTINEZ GARDENING", { x: 415, y, size: 10, font: timesRoman });
+    y -= 15;
+    page.drawText("# de Cédula: ", { x: 415, y, size: 10, font: timesRoman });
+    y -= 15;
+    page.drawText("Invoice Number: " + invoiceNumber, { x: 415, y, size: 10, font: timesRoman });
+
+    // === Tabla de productos ===
+    y -= 70;
+    const headers = ['Type', 'Description', 'Unit', 'Quantity', 'Price'];
+    const colWidths = [100, 200, 50, 50, 80];
+    const rowHeight = 25;
+    let startX = 70;
+    const rowPadding = 10;
+
+    // Dibujar encabezados
+    let currentX = startX;
+    headers.forEach((header, i) => {
       page.drawRectangle({
         x: currentX,
         y: y - rowHeight,
         width: colWidths[i],
         height: rowHeight,
         borderColor: rgb(0, 0, 0),
+        color: rgb(0, 128 / 255, 0),
         borderWidth: 1,
       });
-      page.drawText(cell, { x: currentX + 5, y: y - 18, size: 12 });
+      page.drawText(header, { x: currentX + 5, y: y - 18, size: 12, color: rgb(1.0, 1.0, 1.0), font: timesRomanBold });
       currentX += colWidths[i];
     });
-    y -= rowHeight;
-  });
 
-  // === Totales ===
-  y -= 30;
-  page.drawText("Subtotal: $16,100.00", { x: 440, y, size: 12 });
-  y -= 15;
-  page.drawText("Tax: $2,576.00", { x: 470, y, size: 12 });
-  y -= 15;
-  page.drawText("Total: $18,676.00", { x: 440, y, size: 14, color: rgb(0, 0, 0) });
+   
+    y -= rowHeight;
+    service.serviceDetail.forEach((p: ServiceDetailDto) => {
+      const row = [
+        p.serviceType,
+        p.description,
+        p.unitMeasurement,
+        p.quantity,
+        `$${p.price.toFixed(2)}`,
+      ];
+
+      currentX = startX;
+      row.forEach((cell, i) =>{
+        page.drawRectangle({
+          x: currentX,
+          y: y - rowHeight,
+          width: colWidths[i],
+          height: rowHeight,
+          borderColor: rgb(0, 0, 0),
+          borderWidth: 1,
+        });        
+        page.drawText(cell.toString(), {
+          x: currentX + 5,
+          y: y - 17,
+          size: 9,
+          //font: times,
+        });
+        currentX += colWidths[i];
+      });
+
+      y -= rowHeight;
+    });
+
+    let tax = service.invoice?.payment[0].taxAmount;
+    if (tax == null) { tax = 0; }
+
+    // === Totales ===
+    y -= 30;
+    page.drawText("Subtotal: $" + service.invoice?.subtotalAmount.toFixed(2), { x: 440, y, size: 12, font: timesRoman });
+    y -= 15;
+    page.drawText("Tax:        $" + tax.toFixed(2), { x: 440, y, size: 12, font: timesRoman });
+    y -= 15;
+    page.drawText("Total:     $" + service.invoice?.totalAmount.toFixed(2), { x: 440, y, size: 12, color: rgb(0, 0, 0), font: timesRomanBold });
 
     // Guardar PDF
     const pdfBytes = await pdfDoc.save();
     const blob = new Blob([pdfBytes as unknown as ArrayBuffer], { type: 'application/pdf' });
-
-    //const blob = new Blob([pdfBytes as unknown as ArrayBuffer], { type: 'application/pdf' });   // const blob = new Blob([pdfBytes], { type: 'application/pdf' });
     const url = URL.createObjectURL(blob);
+    const fileName = service.invoice ? service.invoice.invoiceName : 'example.pdf';
 
-     /*document.addEventListener('DOMContentLoaded', () => {
-            // Access document here
-            const a = document.createElement('a');
-    a.href = url;
-    const invoiceName = /*entity.client.name + entity.client.lastName+ '-' +* /anio+'-'+ mesConCeros+'.pdf';
-    a.download = invoiceName;//'factura.pdf';
-    a.click();
-        });*/
-    /*const a = document.createElement('a');
-    a.href = url;
-    const invoiceName = service.client.name + service.client.lastName+ '-' +anio+'-'+ mesConCeros+'.pdf';
-    a.download = invoiceName;//'factura.pdf';
-    a.click();
-    URL.revokeObjectURL(url);
-   // saveAs(blob, 'factura.pdf');*/
-   const fileName = variable.invoice? variable.invoice.invoiceName:'example.pdf';
-   console.log('FILENAME '+fileName);
-const uploadDir: string = path.join(__dirname,'..', 'invoices'); // Or any other desired directory
+    const uploadDir: string = path.join(__dirname, '..', 'invoices'); // Or any other desired directory
 
-// Create the directory if it doesn't exist
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
+    // Create the directory if it doesn't exist
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir, { recursive: true });
+    }
 
-const filePath: string = path.join(uploadDir, fileName);
+    const filePath: string = path.join(uploadDir, fileName);
 
-try {
-    fs.writeFileSync(filePath, pdfBytes);
-    console.log(`PDF document "${fileName}" saved successfully to ${filePath}`);
-} catch (error) {
-    console.error(`Error saving PDF document: ${error}`);
-}
-   return pdfBytes;
+    try {
+      fs.writeFileSync(filePath, pdfBytes);
+      console.log(`PDF document "${fileName}" saved successfully to ${filePath}`);
+    } catch (error) {
+      console.error(`Error saving PDF document: ${error}`);
+    }
+    return pdfBytes;
   }
 }
